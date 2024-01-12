becf3f0214c5e671835da109afe3323b5c5e64a1
GodVov4 <volume245@gmail.com>
2024-01-12 10:41:46 +0200

    add static_root

--- qtable/qtable/settings.py
+++ qtable/qtable/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 LOGIN_REDIRECT_URL = 'qtable_app:index'
 LOGIN_URL = 'users:login'
 LOGOUT_REDIRECT_URL = 'users:login'
