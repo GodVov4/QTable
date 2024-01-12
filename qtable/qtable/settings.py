23718edd514166216c338581c08c733aa7294372
GodVov4 <volume245@gmail.com>
2024-01-12 13:36:17 +0200

    set db

--- qtable/qtable/settings.py
+++ qtable/qtable/settings.py
@@ -79,13 +79,23 @@ WSGI_APPLICATION = 'qtable.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qtable-db',
+        'USER': 'koyeb-adm',
+        'PASSWORD': 'vo8DL1tsECTe',
+        'HOST': 'ep-restless-mud-91122420.eu-central-1.pg.koyeb.app',
+        'OPTIONS': {'sslmode': 'require'},
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
